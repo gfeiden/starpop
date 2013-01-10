80930aac5fe099bda4714f22403046bcf0bb38da
Jessica Lu <jlu@ifa.hawaii.edu>
2013-01-10 12:07:53 -1000

    Added multiplicity.py and updated work on imf.py

---
+++ src/multiplicity.py
@@ -0,0 +1,20 @@
+defaultMFamp = 0.44
+defaultMFindex = 0.51
+defaultCSFamp = 0.50
+defaultCSFindex = 0.45
+defaultCSFmax = 3
+defaultQindex = -0.4
+defaultQmin = 0.01
+default_aMean = 100.0 # log (AU)
+default_aSigma = 0.1  # log (AU)
+
+class Multiplicity(object):
+    def __init__(self, MFamp=defaultMFamp, MFindex=defaultMFindex,
+                 CSFamp=defaultCSFamp, CSFindex=defaultCSFindex,
+                 CSFmax=defaultCSFmax,
+                 Qindex=defaultQindex, Qmin=defaultMultiQmin,
+                 aMean=default_aMean, aSigma=default_aSigma):
+        
+        
+
+
