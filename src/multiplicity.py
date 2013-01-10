966d46ffad24b704df0af9d3edc7cf1b6e128fad
Ka-Hei Law <klaw@precise.lcm.hk>
2013-01-11 06:21:23 +0800

    Move some code to src/multiplicity.py

--- src/multiplicity.py
+++ src/multiplicity.py
@@ -14,7 +14,40 @@ class Multiplicity(object):
                  CSFmax=defaultCSFmax,
                  Qindex=defaultQindex, Qmin=defaultMultiQmin,
                  aMean=default_aMean, aSigma=default_aSigma):
-        
-        
+         
+    #def binary_properties(self, mass, MFamp=defaultMFamp, MFindex=defaultMFindex,
+    #              CSFamp=defaultCSFamp, CSFindex=defaultCSFindex, CSFmax=defaultCSFmax):
+    def getMultiplicityFraction(self):
+        """
+        Given a star's mass, determine the probability that the star is in a
+        multiple system (multiplicity fraction = MF) and its average number of
+        companion stars (companion star fraction = CSF).
+        """
+        # Multiplicity Fraction
+        mf = MFamp * mass**MFindex
+        mf[mf > 1] = 1
 
+    def getCompanionStarFraction(self):
+        """
+        Given a star's mass, determine the probability that the star is in a
+        multiple system (multiplicity fraction = MF) and its average number of
+        companion stars (companion star fraction = CSF).
+        """
+        # Companion Star Fraction
+        csf = CSFamp * mass**CSFindex
+        csf[csf > 3] = CSFmax
+
+        return mf, csf
+
+    def q_cdf_inv(self, x, qLo, beta):
+        """
+        Generative function for companion mass ratio (q = m_comp/m_primary).
+
+        Inputs:
+        x -- random number between 0 and 1.
+        qLo -- lowest possible mass ratio
+        beta -- p(q) \propto q^beta
+        """
+        b = 1.0 + beta
+        return (x * (1.0 - qLo**b) + qLo**b)**(1.0/b)
 
